8883390cc9df0af39b021bdd1037972fe871d203
ElderSam <eldersamuel98@gmail.com>
2022-01-29 16:08:13 -0300

    feat: list labs (projects)

--- src/controllers/LabsController.js
+++ src/controllers/LabsController.js
@@ -1,5 +1,24 @@
+const page1 = require("../../db/mock/DIO_labs/pages/page1.json");
+
 module.exports = {
 	listAll(req, res) {
-		// res.send({ labs });
+		const getNewItem = (item) => {
+			const { level, name, project_type, skills } = item;
+
+			const newObj = {
+				skills,
+				name,
+				project_type,
+				level,
+			};
+			// console.log(newObj);
+			return newObj;
+		};
+
+		const pageRes = page1.results;
+
+		const projects = pageRes.map((project) => getNewItem(project));
+
+		res.send({ projects });
 	},
 };
