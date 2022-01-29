e527a5bdfa741d369c125a78e0346e4b4387b1b2
ElderSam <eldersamuel98@gmail.com>
2022-01-29 15:47:58 -0300

    feat: create the first route with Express

--- src/server.js
+++ src/server.js
@@ -1,1 +1,13 @@
-console.log('Hello Console!!')
+const express = require('express');
+
+const server = express();
+server.use(express.json()) // <==== parse request body as JSON
+
+const routes = express.Router()
+
+routes.get('/', (req, res) => res.send('first route!'))
+
+server.use(routes)
+
+const PORT = 3000;
+server.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
