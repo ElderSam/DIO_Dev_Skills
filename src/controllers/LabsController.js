1fdb854405dc3b7ec80a4f3b5ffae31c14f407ed
ElderSam <eldersamuel98@gmail.com>
2022-01-29 18:26:38 -0300

    feat: labs - organize by project type

--- src/controllers/LabsController.js
+++ src/controllers/LabsController.js
@@ -1,6 +1,6 @@
-let project_types = []
-let arrSkills = []
-let levels = []
+let project_types = [];
+let arrSkills = [];
+let levels = [];
 
 module.exports = {
 	listAll(req, res) {
@@ -15,18 +15,49 @@ module.exports = {
 			};
 		};
 
-		let allProjects = [];
+		const getAllProjects = () => {
+			let allProjects = [];
 
-		for (let i = 1; i <= 13; i++) {
-			const currentPage = require(`../../db/mock/DIO_labs/pages/page${i}.json`);
+			for (let i = 1; i <= 13; i++) {
+				const currentPage = require(`../../db/mock/DIO_labs/pages/page${i}.json`);
 
-			const pageRes = currentPage.results;
+				const pageRes = currentPage.results;
 
-			const projects = pageRes.map((project) => getNewItem(project));
-			allProjects = [...allProjects, ...projects];
+				const projects = pageRes.map((project) => getNewItem(project));
+				allProjects = [...allProjects, ...projects];
+			}
+
+			return allProjects;
+		};
+
+		const groupProjectsByProjectType = (resAllProjects) => {
+			const project_types = [
+				"Full-Stack",
+				"Front-End",
+				"Back-End",
+				"Mobile"
+			];
+	
+			const auxRes = {};
+			project_types.map((type, index) => {
+				array = resAllProjects.filter(item => item.project_type === type)
+	
+				// remote project_type from project
+				array = array.map(item => {
+					delete item.project_type
+					return item
+				})
+				auxRes[project_types[index]] = array
+			})
+
+			return auxRes;
 		}
 
-		res.send({ projects: allProjects });
+		const resAllProjects = getAllProjects();
+
+		const auxRes = groupProjectsByProjectType(resAllProjects);
+
+		res.send({ projects: auxRes });
 	},
 
 	getLevelsAndSkills(req, res) {
@@ -46,8 +77,10 @@ module.exports = {
 			pageRes.map((project) => setItemLevelsAndSkills(project));
 		}
 
-		arrSkills = [...new Set(arrSkills)];
+		arrSkills = [...new Set(arrSkills)].sort();
 		levels = [...new Set(levels)];
-		res.send({ arrSkills, levels });
+		project_types = [...new Set(project_types)];
+
+		res.send({ arrSkills, levels, project_types });
 	},
 };
